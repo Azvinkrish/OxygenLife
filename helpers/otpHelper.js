dcb00a230d05ed9a5e02ea8ea5a579d71a6442df
Azvinkrish <kragaswin@gmail.com>
2022-08-01 21:58:21 +0530

    kk

--- helpers/otpHelper.js
+++ helpers/otpHelper.js
@@ -1,11 +1,11 @@
-
-const client = require('twilio')(config.accountSID,config.authToken)
+require('dotenv').config();
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID,process.env.TWILIO_AUTH_TOKEN)
 
 module.exports={
     make:(phone_number)=>{
         return new Promise(async(resolve,reject)=>{
            await client.verify
-            .services(config.serviceId)
+            .services(process.env.TWILIO_SERVICE_ID)
             .verifications.create({
                 to:`+91${phone_number}`,
                 channel:'sms'
@@ -20,7 +20,7 @@ module.exports={
         return new Promise(async(resolve,reject)=>{
             console.log(phone_number);
            await client.verify
-           .services(config.serviceId)
+           .services(process.env.TWILIO_SERVICE_ID)
            .verificationChecks.create({
                to:`+91${phone_number}`,
                code:otp,
